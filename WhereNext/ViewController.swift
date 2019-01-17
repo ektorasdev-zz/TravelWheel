ef6455f31bea156db3e042d8dfd9b9203a6f81f3
Hector Drandakis <hector.drandakis@gmail.com>
2019-01-17 22:14:54 +0200

    Initial Commit

---
+++ WhereNext/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereNext
+//
+//  Created by Hector Drandakis on 1/17/19.
+//  Copyright © 2019 Hector Drandakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
